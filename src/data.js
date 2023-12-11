964f0e910e881da2ab985b13ea62981f17921913
ArmandoP98 <119010889+ArmandoP98@users.noreply.github.com>
2023-12-11 05:28:50 -0600

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		nameProduct: 'Tenis Nike',
+		price: 800,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/09c5ea6df1bd4be6baaaac5e003e7047_9366/Tenis_Forum_Low_Blanco_FY7756_01_standard.jpg',
+		nameProduct: 'Tenis Adidas',
+		price: 560,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRVT3jY0Am_Q3O8FUdAZcVVuxTDJt0ZLe1kwSRWrH1CKt10zOjs67QepJ32-73fPbVHFD7TGvfPWr6UgwZLyT6pIIyvJ-7W&usqp=CAY',
+		nameProduct: 'Tenis DC',
+		price: 990,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://cdn1.coppel.com/images/catalog/pr/8627892-1.jpg',
+		nameProduct: 'Tenis Vans',
+		price: 780,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://resources.claroshop.com/medios-plazavip/s2/15234/2939868/61080ede48271-016463_1-1600x1600.jpg?scale=350',
+		nameProduct: 'Tenis Charly',
+		price: 2000,
+		quantity: 1,
+	},
+];
